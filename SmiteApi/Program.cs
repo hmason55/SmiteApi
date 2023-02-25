940b21443f7dd2013184f2d633a02e26b57b8288
Hunter <hmason55@gmail.com>
2023-02-24 19:40:51 -0800

    Initial commit.

---
+++ SmiteApi/Program.cs
@@ -0,0 +1,62 @@
+﻿using SmiteCommon.Models.Api;
+using SmiteCommon.Models.Items;
+using System.Text.Json;
+
+namespace SmiteApi;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        if(args.Length < 3)
+        {
+            Console.WriteLine("Please provide the following: <devId> <authKey> <apiQuery>");
+        }
+
+        Developer developer = new()
+        {
+            Id = args[0],
+            AuthKey = args[1]
+        };
+
+        Configuration configuration = Configuration.Load();
+        Client client = new(developer, configuration.Session);
+        await client.CheckSession(configuration);
+
+        string query = args[2];
+        string path = args.Length > 3 ? args[3] : null;
+        object json = null;
+        switch (query)
+        {
+            case Paths.GetItems:
+                json = await client.GetItems(configuration.Language);
+                break;
+
+            case Paths.GetGods:
+                json = await client.GetGods(configuration.Language);
+                break;
+
+            default:
+                Console.WriteLine($"Unknown query: {query}");
+                break;
+        }
+
+        if(path != null && json != null)
+        {
+            try
+            {
+                string directory = Path.GetDirectoryName(path);
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+
+                File.WriteAllText(path, JsonSerializer.Serialize(json, Configuration.JsonSerializerOptions));
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
